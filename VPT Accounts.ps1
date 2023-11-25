aa0b26770f5556dd04cd2b998816448c096634f5
VoNgocTu <tu.vongoc@axonactive.com>
2023-11-25 09:30:36 +0700

    Refactor

--- VPT Accounts.ps1
+++ VPT Accounts.ps1
@@ -1,100 +1,117 @@
+Add-Type -AssemblyName UIAutomationClient
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
+# Correct root when run as exe
 $root = $PSScriptRoot
 if ($root -eq "")
 {
     $root = ".";
 }
 
+$scripts = "$root\scripts"
+$images = "$root\images"
+$tools = "$root\tools"
+$buttonHeight = 30
+
 $VPTAccounts                     = New-Object system.Windows.Forms.Form
 $VPTAccounts.ClientSize          = New-Object System.Drawing.Point(265, 500)
-$VPTAccounts.MaximumSize         = New-Object System.Drawing.Size(265, 500)
 $VPTAccounts.Location = New-Object System.Drawing.Point(30, 20)
+# $VPTAccounts.MaximumSize         = New-Object System.Drawing.Size(265, 500)
 #$VPTAccounts.MaximumSize         = New-Object System.Drawing.Size(900, 476)
 $VPTAccounts.text                = "VPT Accounts"
 $VPTAccounts.TopMost             = $false
 
-# Create two tab pages
-$main = New-Object System.Windows.Forms.TabPage
-$main.Text = "Main"
-$main.BackgroundImage = [system.drawing.image]::FromFile("$root\image\Background.jpg")
 
-$tuClone = New-Object System.Windows.Forms.TabPage
-$tuClone.Text = "Tú C"
-$tuClone.BackgroundImage = [system.drawing.image]::FromFile("$root\image\Background.jpg")
-
-$tanClone = New-Object System.Windows.Forms.TabPage
-$tanClone.Text = "Tân C"
-$tanClone.BackgroundImage = [system.drawing.image]::FromFile("$root\image\Background.jpg")
-
-$khoaClone = New-Object System.Windows.Forms.TabPage
-$khoaClone.Text = "Khoa C"
-$khoaClone.BackgroundImage = [system.drawing.image]::FromFile("$root\image\Background.jpg")
-
-$yuiClone = New-Object System.Windows.Forms.TabPage
-$yuiClone.Text = "Yui C"
-$yuiClone.BackgroundImage = [system.drawing.image]::FromFile("$root\image\Background.jpg")
+# $accList = $(Invoke-RestMethod -Uri https://raw.githubusercontent.com/VoNgocTu/VPT/main/accounts.json).accList
+$tabList = $(Get-Content ".\accounts.json" | Out-String | ConvertFrom-Json).tabList
 
 # Add the tab pages to the tab control
 $tabControl = New-Object System.Windows.Forms.TabControl
-$tabControl.Controls.Add($main)
-$tabControl.Controls.Add($tuClone)
-$tabControl.Controls.Add($tanClone)
-$tabControl.Controls.Add($khoaClone)
-$tabControl.Controls.Add($yuiClone)
 $VPTAccounts.Controls.Add($tabControl)
-
 $tabControl.Dock = "Fill"
 
 
-$accList = $(Invoke-RestMethod -Uri https://raw.githubusercontent.com/VoNgocTu/VPT/main/accounts.json).accList
-
-$BugOnlineButton                         = New-Object system.Windows.Forms.Button
-$BugOnlineButton.text                    = "Bug online"
-$BugOnlineButton.width                   = 180
-$BugOnlineButton.height                  = 30
-$BugOnlineButton.location                = New-Object System.Drawing.Point(30, 20)
-$BugOnlineButton.Font                    = New-Object System.Drawing.Font('Times New Roman',10,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
-$BugOnlineButton.BackColor               = [System.Drawing.ColorTranslator]::FromHtml("#a5e6d6")
-$BugOnlineButton.Add_Click({ Start-Process "$root\script\Auto-Bug-Online.ahk" })
-
-$ResetAttackButton                         = New-Object system.Windows.Forms.Button
-$ResetAttackButton.text                    = "Reset lượt đánh"
-$ResetAttackButton.width                   = 180
-$ResetAttackButton.height                  = 30
-$ResetAttackButton.location                = New-Object System.Drawing.Point(30, 60)
-$ResetAttackButton.Font                    = New-Object System.Drawing.Font('Times New Roman',10,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
-$ResetAttackButton.BackColor               = [System.Drawing.ColorTranslator]::FromHtml("#a5e6d6")
-$ResetAttackButton.Add_Click({ Start-Process "$root\script\Reset-Auto-Attack-Number.ahk" })
-$main.controls.AddRange(@($BugOnlineButton, $ResetAttackButton))
-
-foreach ($acc in $accList) 
-{
-    $LoginButton                         = New-Object system.Windows.Forms.Button
-    $LoginButton.text                    = "$($acc.owner)  -  $($acc.name)  -  $($acc.char)"
-    $LoginButton.width                   = 150
-    $LoginButton.height                  = 30
-    $LoginButton.location                = New-Object System.Drawing.Point(30, $($acc.index * 40 + 60))
-    $LoginButton.Font                    = New-Object System.Drawing.Font('Times New Roman',10,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
-    $LoginButton.BackColor               = [System.Drawing.ColorTranslator]::FromHtml("#a5e6d6")
-    $LoginButton.Tag                     = $acc.link
-    $LoginButton.Add_Click({ openAccount $this.Tag })
-
-    $CopyLinkButton                       = New-Object system.Windows.Forms.Button
-    $CopyLinkButton.image                 = [system.drawing.image]::FromFile("$root\image\link-icon.png")
-    $CopyLinkButton.width                 = 30
-    $CopyLinkButton.height                = 30
-    $CopyLinkButton.location              = New-Object System.Drawing.Point(180, $($acc.index * 40 + 60))
-    $CopyLinkButton.Tag                   = $acc.link
-    $CopyLinkButton.Add_Click({ copyLink $this.Tag })
-    
-    $main.controls.AddRange(@($LoginButton, $CopyLinkButton))
+function getButtonX ($index) {
+    return 30
 }
 
+function getButtonY ($index) {
+    return $index * 40 + 20
+}
+
+function getButtonWidth ($index) {
+    return 180
+}
+
+foreach($tab in $tabList) {
+    $tabPage = New-Object System.Windows.Forms.TabPage
+    $tabPage.Text = $tab.owner
+    $tabPage.BackgroundImage = [system.drawing.image]::FromFile("$images\Background.jpg")
+    $tabControl.Controls.Add($tabPage)
+    
+    $buttonIndex = 0
+    if ($tab.owner -eq "Main") {
+        $BugOnlineButton                         = New-Object system.Windows.Forms.Button
+        $BugOnlineButton.text                    = "Bug online"
+        $BugOnlineButton.width                   = $(getButtonWidth $buttonIndex)
+        $BugOnlineButton.height                  = $buttonHeight
+        $BugOnlineButton.location                = New-Object System.Drawing.Point($(getButtonX $buttonIndex), $(getButtonY $buttonIndex))
+        $BugOnlineButton.Font                    = New-Object System.Drawing.Font('Times New Roman',10,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+        $BugOnlineButton.BackColor               = [System.Drawing.ColorTranslator]::FromHtml("#a5e6d6")
+        $BugOnlineButton.Add_Click({ Start-Process "$scripts\Auto-Bug-Online.ahk" })
+        $buttonIndex++
+        
+        $ResetAttackButton                         = New-Object system.Windows.Forms.Button
+        $ResetAttackButton.text                    = "Reset lượt đánh"
+        $ResetAttackButton.width                   = $(getButtonWidth $buttonIndex)
+        $ResetAttackButton.height                  = $buttonHeight
+        $ResetAttackButton.location                = New-Object System.Drawing.Point($(getButtonX $buttonIndex), $(getButtonY $buttonIndex))
+        $ResetAttackButton.Font                    = New-Object System.Drawing.Font('Times New Roman',10,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+        $ResetAttackButton.BackColor               = [System.Drawing.ColorTranslator]::FromHtml("#a5e6d6")
+        $ResetAttackButton.Add_Click({ Start-Process "$scripts\Reset-Auto-Attack-Number.ahk" })
+        $buttonIndex++
+        $tabPage.controls.AddRange(@($BugOnlineButton, $ResetAttackButton))
+    }
+    
+    foreach ($acc in $tab.accList) 
+    {
+        $LoginButton                         = New-Object system.Windows.Forms.Button
+        $LoginButton.text                    = "$($acc.char)  -  $($acc.name)"
+        $LoginButton.width                   = $(getButtonWidth $buttonIndex)
+        $LoginButton.height                  = $buttonHeight
+        $LoginButton.location                = New-Object System.Drawing.Point($(getButtonX $buttonIndex), $(getButtonY $buttonIndex))
+        $LoginButton.Font                    = New-Object System.Drawing.Font('Times New Roman',10,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+        $LoginButton.BackColor               = [System.Drawing.ColorTranslator]::FromHtml("#a5e6d6")
+        $LoginButton.Tag                     = $acc
+        $LoginButton.Add_Click({ openAccount $this })
+        $buttonIndex++
+    
+        # $CopyLinkButton                       = New-Object system.Windows.Forms.Button
+        # $CopyLinkButton.image                 = [system.drawing.image]::FromFile("$images\link-icon.png")
+        # $CopyLinkButton.width                 = 30
+        # $CopyLinkButton.height                = 30
+        # $CopyLinkButton.location              = New-Object System.Drawing.Point(180, $($acc.index * 40 + 60))
+        # $CopyLinkButton.Tag                   = $acc.link
+        # $CopyLinkButton.Add_Click({ copyLink $this.Tag })
+        
+        $tabPage.controls.AddRange(@($LoginButton))
+    }
+}
+
+
+
 #region Logic 
-function openAccount ($link) {          
-    .\flashplayer_32.exe $(getFlashLink $link)
+function openAccount ($button) {
+    if ($null -eq $button.Tag.processId) {
+        $button.Tag.processId = $(Start-Process .\flashplayer_32.exe $(getFlashLink $button.Tag.link) -passthru).ID
+    } else {
+        if ($(Get-Process -Id $button.Tag.processId).ProcessName -eq "flashplayer_32") {
+            toggleGameWindow $button.Tag.processId
+        } else {
+            $button.Tag.processId = $(Start-Process .\flashplayer_32.exe $(getFlashLink $button.Tag.link) -passthru).ID
+        }
+    }
 }
 
 function copyLink ($link) {
@@ -102,12 +119,29 @@ function copyLink ($link) {
 }
 
 function getFlashLink ($link) {
-    $link -match '.*\/s\/(.*)\/index.php&(.*)&(.*)' | findstr abc
-    # $link -match '.*\/s\/(.*)\/GameLoaders.swf\?user=(.*)&pass=(.*)&.*' | findstr abc
+    $link -match '.*\/s\/(.*)\/index.php&(.*)&(.*)' | findstr abcdef
+    if ($matches -eq $null) {
+        # $link -match '.*\/s\/(.*)\/GameLoaders.swf\?user=(.*)&pass=(.*)&.*' | findstr abc
+        return $link
+    }
+    
     $server = $matches[1]
     $user = $matches[2]
     $pass = $matches[3]
     return "http://s3.vuaphapthuat.goplay.vn/s/$server/GameLoaders.swf?user=$user&pass=$pass&isExpand=true" 
 }
+
+
+function toggleGameWindow($processId) {
+    $MyProcess = Get-Process -Id $processId
+    $ae = [System.Windows.Automation.AutomationElement]::FromHandle($MyProcess.MainWindowHandle)
+    $wp = $ae.GetCurrentPattern([System.Windows.Automation.WindowPatternIdentifiers]::Pattern)
+    if ($wp.Current.WindowVisualState -eq 'Minimized') { 
+        $wp.SetWindowVisualState('Normal') 
+    } else { 
+        $wp.SetWindowVisualState('Minimized') 
+    }
+}
+
 
 [void]$VPTAccounts.ShowDialog() 
---
+++ scripts/Auto-Farm-1.ahk
@@ -0,0 +1,122 @@
+pidArray := []
+
+F9::{
+    winTitle := "Adobe Flash Player 32"
+    pid := WinActive(winTitle)
+
+    global pidArray
+    pidArray.Push(pid)
+}
+
+F10::{
+    global pidArray
+    ahk_id := "ahk_id " pidArray.get(1)
+    while (true) {
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        regen(ahk_id)
+
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        regen(ahk_id)
+        
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        regen(ahk_id)
+        
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        regen(ahk_id)
+        
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        regen(ahk_id)
+        
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        regen(ahk_id)
+        
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        regen(ahk_id)
+        
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        move(ahk_id)
+        regen(ahk_id)
+        
+        Sleep 7000
+        resetAutoAttack()
+    }
+}
+
+
+move(ahk_id)
+{
+    ControlClick "x211 y555", ahk_id
+    Sleep 2000
+    ControlClick "x311 y555", ahk_id
+    Sleep 2000
+}
+
+stop(ahk_id)
+{
+    ControlClick "x521 y351", ahk_id
+}
+
+resetAutoAttack() {
+    global pidArray
+    ids := pidArray
+    for id in ids
+    {
+        ControlClick "x1026 y608", "ahk_id " id
+        ControlClick "x1026 y608", "ahk_id " id
+    }
+}
+
+regen(main_id, sleepTime := 200) {
+    Sleep 5000 ; Wait for combat end
+    stop(main_id)
+
+    global pidArray
+    ids := pidArray
+    for id in ids
+    {
+        ahk_id := "ahk_id " id
+        ControlClick "x124 y23", ahk_id ; Bơm máu mana nhân vật
+        Sleep sleepTime
+        ControlClick "x93 y81", ahk_id ; Bơm máu mana pet
+        Sleep sleepTime
+        ControlClick "x103 y81", ahk_id ; Bơm máu mana pet
+        Sleep sleepTime
+        ControlClick "x113 y81", ahk_id ; Bơm máu mana pet
+        Sleep sleepTime
+        ControlClick "x133 y81", ahk_id ; Bơm máu mana pet
+        Sleep sleepTime
+        ControlClick "x133 y81", ahk_id ; Bơm máu mana pet
+        Sleep sleepTime
+    }
+}
